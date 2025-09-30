e0658a0a4f1de473b5a286dc7c6bb67c76ed8d3d
minkkjean <mkkjin@g.shingu.ac.kr>
2025-09-30 13:31:37 +0900

    Feat: 프로젝타일 코드

---
+++ GEA_project/Assets/Scripts/Projectile.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    public float speed = 20f;
+    public float lifeTime = 2f;
+
+
+    void Start()
+    {
+        Destroy(gameObject, lifeTime);
+
+    }
+
+
+    void Update()
+    {
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Enemy"))
+        {
+            Destroy(other.gameObject);
+            Destroy(gameObject);
+        }
+    }
+}
